24e5ccc95acc0dc634b20ec4ef3a22e1d66ed049
vitalets <noginsk@rambler.ru>
2025-02-16 14:15:08 +0400

    examples: add nuxt

---
+++ examples/webdriverio-nuxt/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { data: users } = useFetch('https://jsonplaceholder.typicode.com/users');
+</script>
+
+<template>
+  <h2>Users:</h2>
+  <ul>
+    <li v-for="user in users" :key="user.id">
+      {{ user.name }}
+    </li>
+  </ul>
+</template>
