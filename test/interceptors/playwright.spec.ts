2bd43bd5f979fd26cfd5216496bca5a254ab18aa
vitalets <noginsk@rambler.ru>
2025-03-02 17:07:26 +0400

    test: split slow test

--- test/interceptors/playwright.spec.ts
+++ test/interceptors/playwright.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, beforeAll, beforeEach, afterAll } from 'vitest';
+import { describe, test, expect, beforeAll, beforeEach, afterAll } from 'vitest';
 import { Browser, BrowserContext, chromium } from 'playwright';
 import { MockClient } from '../../src';
 import { setupPlaywrightInterceptor } from '../../src/interceptors/playwright';
@@ -8,50 +8,52 @@ const mockClient = new MockClient();
 let browser: Browser;
 let context: BrowserContext;
 
-beforeAll(async () => {
-  browser = await chromium.launch();
-  context = await browser.newContext();
-});
-
-afterAll(async () => {
-  await context.close();
-  await context.close();
-});
-
-beforeEach(() => {
-  mockClient.reset();
-});
-
-test('mock response', async () => {
-  const page = await context.newPage();
-  await setupPlaywrightInterceptor(page, mockClient);
-
-  await mockClient.GET('https://jsonplaceholder.typicode.com/users', {
-    body: [{ id: 1, name: 'John Smith' }],
+describe.skipIf(!process.env.SLOW)('playwright', () => {
+  beforeAll(async () => {
+    browser = await chromium.launch();
+    context = await browser.newContext();
   });
 
-  await page.goto('https://example.com');
-  const res = await page.evaluate(() =>
-    fetch('https://jsonplaceholder.typicode.com/users').then((r) => r.json()),
-  );
-
-  expect(res).toEqual([{ id: 1, name: 'John Smith' }]);
-});
-
-test('patch response', async () => {
-  const page = await context.newPage();
-  await setupPlaywrightInterceptor(page, mockClient);
-
-  await mockClient.GET('https://jsonplaceholder.typicode.com/users', {
-    bodyPatch: {
-      '[0].name': 'John Smith',
-    },
+  afterAll(async () => {
+    await context.close();
+    await context.close();
   });
 
-  await page.goto('https://example.com');
-  const res = await page.evaluate(() =>
-    fetch('https://jsonplaceholder.typicode.com/users').then((r) => r.json()),
-  );
+  beforeEach(() => {
+    mockClient.reset();
+  });
 
-  expect(res[0].name).toEqual('John Smith');
+  test('mock response', async () => {
+    const page = await context.newPage();
+    await setupPlaywrightInterceptor(page, mockClient);
+
+    await mockClient.GET('https://jsonplaceholder.typicode.com/users', {
+      body: [{ id: 1, name: 'John Smith' }],
+    });
+
+    await page.goto('https://example.com');
+    const res = await page.evaluate(() =>
+      fetch('https://jsonplaceholder.typicode.com/users').then((r) => r.json()),
+    );
+
+    expect(res).toEqual([{ id: 1, name: 'John Smith' }]);
+  });
+
+  test('patch response', async () => {
+    const page = await context.newPage();
+    await setupPlaywrightInterceptor(page, mockClient);
+
+    await mockClient.GET('https://jsonplaceholder.typicode.com/users', {
+      bodyPatch: {
+        '[0].name': 'John Smith',
+      },
+    });
+
+    await page.goto('https://example.com');
+    const res = await page.evaluate(() =>
+      fetch('https://jsonplaceholder.typicode.com/users').then((r) => r.json()),
+    );
+
+    expect(res[0].name).toEqual('John Smith');
+  });
 });
