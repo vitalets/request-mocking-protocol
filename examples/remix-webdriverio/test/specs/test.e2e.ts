e08ef165914344092876f58a72310f77912bf372
vitalets <noginsk@rambler.ru>
2025-03-07 09:46:55 +0400

    examples: remix

--- examples/remix-webdriverio/test/specs/test.e2e.ts
+++ examples/remix-webdriverio/test/specs/test.e2e.ts
@@ -1,14 +1,14 @@
 import { browser, $, expect } from '@wdio/globals';
-import { mockRemoteRequest } from 'request-mocking-protocol';
+import { MockClient } from 'request-mocking-protocol';
 
 describe('homepage', () => {
   it('should show users', async () => {
-    const mockRemoteRequest = new mockRemoteRequest();
-    mockRemoteRequest.addMock('https://jsonplaceholder.typicode.com/users', {
+    const mockClient = new MockClient();
+    mockClient.addMock('https://jsonplaceholder.typicode.com/users', {
       body: JSON.stringify([{ id: 1, name: 'John Smith' }]),
     });
 
-    await browser.url('/', { headers: mockRemoteRequest.headers });
+    await browser.url('/', { headers: mockClient.headers });
 
     await expect($('li')).toHaveText('John Smith');
   });
