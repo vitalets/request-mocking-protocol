bdf688bfbce50f73431fe8a894cbeb731230438b
vitalets <noginsk@rambler.ru>
2025-03-07 09:38:15 +0400

    examples: next

--- examples/nextjs-playwright/src/instrumentation.ts
+++ examples/nextjs-playwright/src/instrumentation.ts
@@ -2,10 +2,7 @@ import { headers } from 'next/headers';
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV !== 'production') {
-    const { setupServer } = await import('msw/node');
-    const { createHandler } = await import('request-mocking-protocol/msw');
-
-    const server = setupServer(createHandler(() => headers()));
-    server.listen();
+    const { setupFetchInterceptor } = await import('request-mocking-protocol/fetch');
+    setupFetchInterceptor(() => headers());
   }
 }
--- examples/nextjs-playwright/test/fixtures.ts
+++ examples/nextjs-playwright/test/fixtures.ts
@@ -1,10 +1,10 @@
 import { test as base } from '@playwright/test';
-import { MockRemoteRequest } from 'request-mocking-protocol';
+import { MockClient } from 'request-mocking-protocol';
 
-export const test = base.extend<{ mockRemoteRequest: MockRemoteRequest }>({
-  mockRemoteRequest: async ({ context }, use) => {
-    const mockRemoteRequest = new MockRemoteRequest();
-    mockRemoteRequest.onChange = async (headers) => context.setExtraHTTPHeaders(headers);
-    await use(mockRemoteRequest);
+export const test = base.extend<{ mockServerRequest: MockClient }>({
+  mockServerRequest: async ({ context }, use) => {
+    const mockClient = new MockClient();
+    mockClient.onChange = async (headers) => context.setExtraHTTPHeaders(headers);
+    await use(mockClient);
   },
 });
