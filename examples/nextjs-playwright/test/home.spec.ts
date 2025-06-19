cd37a725ea3f78ad1a05f8fda95a16009a69ffa5
vitalets <noginsk@rambler.ru>
2025-06-19 11:20:27 +0400

    improve build to fix turbopack example

--- examples/nextjs-playwright/test/home.spec.ts
+++ examples/nextjs-playwright/test/home.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from './fixtures';
 
-test('show show users on the homepage', async ({ page, mockServerRequest }) => {
+test('show users on the homepage', async ({ page, mockServerRequest }) => {
   await mockServerRequest.GET('https://jsonplaceholder.typicode.com/users', {
     body: [{ id: 1, name: 'John Smith' }],
   });
--- scripts/build.sh
+++ scripts/build.sh
@@ -11,3 +11,12 @@ rm -rf examples/node_modules/request-mocking-protocol
 mkdir -p examples/node_modules/request-mocking-protocol
 cp -R ./dist examples/node_modules/request-mocking-protocol/
 cp ./package.json examples/node_modules/request-mocking-protocol/package.json
+
+# also copy to 'examples/nextjs-playwright/node_modules/'
+# because Next.js in turbopack mode can't import from upper dir (for some reason)
+rm -rf examples/nextjs-playwright/node_modules/request-mocking-protocol
+mkdir -p examples/nextjs-playwright/node_modules/request-mocking-protocol
+cp -R ./dist examples/nextjs-playwright/node_modules/request-mocking-protocol/
+cp ./package.json examples/nextjs-playwright/node_modules/request-mocking-protocol/package.json
+# also copy dependencies
+cp -R ./node_modules/lodash ./node_modules/urlpattern-polyfill examples/nextjs-playwright/node_modules/
